88bf1099aa3a69c376e23b9485269b45068c8d5d
Antonio K <antonio@172-26-23-73.eduroam.bath.ac.uk>
2026-01-21 22:18:29 +0000

    add_expense pseudo-code outline

--- expenses.py
+++ expenses.py
@@ -1,5 +1,12 @@
 def add_expense():
     print("Add expense — not implemented yet")
+    # 1. Ask user for amount
+    # 2. Ask user for durrency
+    # 3. Ask user for category
+    # 4. Ask user for date (optional)
+    # 5. Store details in a dictionary or database
+    #6. Print confirmation message
+
 
 
 def view_expenses():
