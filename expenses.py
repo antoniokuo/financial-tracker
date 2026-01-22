6651513fdfd3bead64d3bdd887a81fdf7e45afac
Antonio K <antonio@172-26-23-73.eduroam.bath.ac.uk>
2026-01-22 22:51:00 +0000

    Add optional date input to add_expense

--- expenses.py
+++ expenses.py
@@ -4,7 +4,7 @@ def add_expense():
     amount = input("Enter expense amount: ")
     try:
         amount = float(amount)
-    except:
+    except ValueError:
         print("Invalid amount. Please enter a number.")
         return
     print(f"Amount entered: {amount}")
@@ -12,10 +12,21 @@ def add_expense():
     # 2. Ask user for durrency
     currency = input("Enter currency (e.g., USD, GBP): ")
     print(f"Currency entered: {currency}")
-    
+
     # 3. Ask user for category
+    category = input("Enter category (press enter to skip): ")
+    if category == "":
+        category = "Miscellaneouus"
+    print(f"{amount}, {currency}, {category}")
+
     # 4. Ask user for date (optional)
+    date = input("Enter date (press enter to skip): ")
+    if date == "":
+        date = "Not specified"
+    print(f"{amount}, {currency}, {category}, {date}")
+    
     # 5. Store details in a dictionary or database
+
     #6. Print confirmation message
 
 
